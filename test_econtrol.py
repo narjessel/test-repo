fffbfa7101832be449a35349748e2cde79ff6e13
Narjess <41012127+narjessel@users.noreply.github.com>
2023-11-07 16:50:31 +0100

    Update test_econtrol.py

--- test_econtrol.py
+++ test_econtrol.py
@@ -8,6 +8,8 @@ from time import sleep
  
 def test_econtrol():
     options = webdriver.ChromeOptions()
+    options.addArguments("--no-sandbox");
+    options.addArguments("--disable-dev-shm-usage");
     options.add_experimental_option('excludeSwitches', ['enable-logging'])
     chrome_driver = webdriver.Chrome(options=options)
     
