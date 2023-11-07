7cfe3928e4828ecb4b0222b1410ba05da4f4facc
Narjess <41012127+narjessel@users.noreply.github.com>
2023-11-07 18:04:50 +0100

    Update test_econtrol.py

--- test_econtrol.py
+++ test_econtrol.py
@@ -1,37 +1,27 @@
 import pytest
-from selenium import webdriver
 import sys
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from time import sleep
- 
-def test_econtrol():
-    options = webdriver.ChromeOptions()
-    options.add_argument("--headless")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--ignore-certificate-errors")
-    options.add_argument("--disable-extensions")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_experimental_option('excludeSwitches', ['enable-logging'])
-    chrome_driver = webdriver.Chrome(options=options)
-    
-    chrome_driver.get('http://192.168.10.44:8080/econtrol/login')
-    chrome_driver.maximize_window()
- 
-    username = "admin"
-    password = "econtrol"
 
-    username_text_field= chrome_driver.find_element(By.ID, "login")
-    username_text_field.send_keys(username)
-
-    password_text_field= chrome_driver.find_element(By.ID, "passe")
-    password_text_field.send_keys(password)
-
-    sleep(5)
- 
-    chrome_driver.find_element(By.XPATH, "//button[@type='submit']").click()
-    sleep(5)
- 
-    chrome_driver.close()
+@pytest.mark.usefixtures("setup")
+class TestEcontrolExample:
+     def test_econtrol():
+         chrome_driver.get('http://192.168.10.44:8080/econtrol/login')
+         chrome_driver.maximize_window()
+      
+         username = "admin"
+         password = "econtrol"
+     
+         username_text_field= chrome_driver.find_element(By.ID, "login")
+         username_text_field.send_keys(username)
+     
+         password_text_field= chrome_driver.find_element(By.ID, "passe")
+         password_text_field.send_keys(password)
+     
+         sleep(5)
+      
+         chrome_driver.find_element(By.XPATH, "//button[@type='submit']").click()
+         sleep(5)
+      
+         chrome_driver.close()
