ec6b0db733f76674c926b61c512d29420bd336ec
Narjess <41012127+narjessel@users.noreply.github.com>
2023-11-07 17:36:27 +0100

    Update test_econtrol.py

--- test_econtrol.py
+++ test_econtrol.py
@@ -9,6 +9,9 @@ from time import sleep
 def test_econtrol():
     options = webdriver.ChromeOptions()
     options.add_argument("--headless")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--ignore-certificate-errors")
+    options.add_argument("--disable-extensions")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_experimental_option('excludeSwitches', ['enable-logging'])
