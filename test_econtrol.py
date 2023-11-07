3b027d8355f833992822b77edc446eec507d2dc5
Narjess <41012127+narjessel@users.noreply.github.com>
2023-11-07 16:16:05 +0100

    Create test_econtrol.py

---
+++ test_econtrol.py
@@ -0,0 +1,31 @@
+import pytest
+from selenium import webdriver
+import sys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from time import sleep
+ 
+def test_lambdatest_todo_app():
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    chrome_driver = webdriver.Chrome(options=options)
+    
+    chrome_driver.get('http://192.168.10.44:8080/econtrol/login')
+    chrome_driver.maximize_window()
+ 
+    username = "admin"
+    password = "econtrol"
+
+    username_text_field= chrome_driver.find_element(By.ID, "login")
+    username_text_field.send_keys(username)
+
+    password_text_field= chrome_driver.find_element(By.ID, "passe")
+    password_text_field.send_keys(password)
+
+    sleep(5)
+ 
+    chrome_driver.find_element(By.XPATH, "//button[@type='submit']").click()
+    sleep(5)
+ 
+    chrome_driver.close()
