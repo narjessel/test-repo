fefb76eeb558ef10118200d4d1ff09c214c678ea
Narjess <41012127+narjessel@users.noreply.github.com>
2023-11-08 12:14:56 +0100

    Update test_econtrol.py

--- test_econtrol.py
+++ test_econtrol.py
@@ -7,21 +7,21 @@ from time import sleep
 @pytest.mark.usefixtures("setup")
 class TestEcontrolExample:
      def test_econtrol(self):
-         chrome_driver.get('http://192.168.10.44:8080/econtrol/login')
-         chrome_driver.maximize_window()
+         self.driver.get('http://192.168.10.44:8080/econtrol/login')
+         self.driver.maximize_window()
       
          username = "admin"
          password = "econtrol"
      
-         username_text_field= chrome_driver.find_element(By.ID, "login")
+         username_text_field= self.driver.find_element(By.ID, "login")
          username_text_field.send_keys(username)
      
-         password_text_field= chrome_driver.find_element(By.ID, "passe")
+         password_text_field= self.driver.find_element(By.ID, "passe")
          password_text_field.send_keys(password)
      
          sleep(5)
       
-         chrome_driver.find_element(By.XPATH, "//button[@type='submit']").click()
+         self.driver.find_element(By.XPATH, "//button[@type='submit']").click()
          sleep(5)
       
          #chrome_driver.close()
