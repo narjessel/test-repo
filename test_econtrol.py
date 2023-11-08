cfe729949a4ca2324ec52b4dd821322b982dd5a0
Narjess <41012127+narjessel@users.noreply.github.com>
2023-11-08 17:35:48 +0100

    Update test_econtrol.py

--- test_econtrol.py
+++ test_econtrol.py
@@ -7,8 +7,9 @@ from time import sleep
 @pytest.mark.usefixtures("setup")
 class TestEcontrolExample:
     def test_econtrol(self):
+        sleep(3)
         self.driver.get('http://192.168.10.44:8080/econtrol/login')
-        sleep(5)
+        sleep(3)
       
         username = "admin"
         password = "econtrol"
