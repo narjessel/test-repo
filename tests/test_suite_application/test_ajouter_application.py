d65697c0b4575bc333e88817cd507b78adad175f
Narjess <41012127+narjessel@users.noreply.github.com>
2023-12-07 18:33:46 +0100

    Update test_ajouter_application.py

--- tests/test_suite_application/test_ajouter_application.py
+++ tests/test_suite_application/test_ajouter_application.py
@@ -2,6 +2,7 @@ import pytest
 import sys
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
 from time import sleep
 
 @pytest.mark.usefixtures("setup")
