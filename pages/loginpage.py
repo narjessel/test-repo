53831bf127b445da48820bd81f6fb4515f31f0ae
Narjess <41012127+narjessel@users.noreply.github.com>
2023-12-07 19:01:59 +0100

    Create loginpage.py

---
+++ pages/loginpage.py
@@ -0,0 +1,22 @@
+import pytest
+import sys
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from time import sleep
+
+@pytest.mark.usefixtures("setup")
+class TestLoginPage:
+  def test_simple_authentification(self):
+      print("Authentification test")
+      self.driver.get('https://ectest.somone.fr/econtrol/login/')
+    
+      username = "admin"
+      password = "econtrolsomone2023"
+  
+      username_text_field= self.driver.find_element(By.ID, "login")
+      username_text_field.send_keys(username)
+  
+      password_text_field= self.driver.find_element(By.ID, "passe")
+      password_text_field.send_keys(password)
+    
+      self.driver.find_element(By.XPATH, "//button[@type='submit']").click()
